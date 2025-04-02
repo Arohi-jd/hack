db97095724050968f566f40eff992ff16d212784
Arohi Jadhav <arohijadhav172@gmail.com>
2025-04-02 13:16:33 +0530

    Update PastHackathons.vue
    a

---
+++ src/pages/PastHackathons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Past Hackathons</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PastHackathons"
+};
+</script>
+
+<style scoped>
+/* Add styles if necessary */
+</style>
